8d9196241276f436a42bb0d06a72dd2c804f45d4
PC-354\Administrator <bubble1540@naver.com>
2024-11-09 18:21:53 +0900

    player move/idle animation 구현

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private JoyStick _joyStick;
 
     [SerializeField] private GameObject player;
+    private JoyStick joyStick;
+    public Animator Ani;
+    private bool isMoving;
 
-    private bool _isMoving;
+    public void Start()
+    {
+        Ani = GetComponentInChildren<Animator>();
+    }
 
     private void Update()
     {
@@ -17,36 +22,42 @@ public class PlayerMovement : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    _isMoving = true;
+                    isMoving = true;
                     break;
                 case TouchPhase.Ended:
-                    _isMoving = false;
+                    isMoving = false;
                     // idle 애니메이션
+                    Debug.Log("Ended");
+                    Ani.SetBool("player_idle", true);
+                    Ani.SetBool("player_walk", false);
                     break;
                 case TouchPhase.Moved:
                     // move 애니메이션
+                    Debug.Log("Moved");
+                    Ani.SetBool("player_idle", false);
+                    Ani.SetBool("player_walk", true);
                     break;
             }
         }
         else
         {
-            _isMoving = false;
+            isMoving = false;
         }
     }
 
     private void FixedUpdate()
     {
-        if (!_isMoving) return;
-        var moveX = _joyStick.Horizontal * moveSpeed * Time.deltaTime;
-        var moveZ = _joyStick.Vertical * moveSpeed * Time.deltaTime;
+        if (!isMoving) return;
+        var moveX = joyStick.Horizontal * moveSpeed * Time.deltaTime;
+        var moveZ = joyStick.Vertical * moveSpeed * Time.deltaTime;
 
         var movement = new Vector3(moveX, 0, moveZ);
         transform.Translate(movement, Space.World);
         player.transform.rotation = Quaternion.LookRotation(movement);
     }
 
-    public void SetJoyStick(JoyStick joyStick)
+    public void SetJoyStick(JoyStick _joyStick)
     {
-        _joyStick = joyStick;
+        joyStick = _joyStick;
     }
 }
